7b553dee986624aa5c6f44643019e46373487626
yadhukrishnan8 <yadhukrishnan8080@gmail.com>
2026-05-03 00:06:49 +0530

    fix: add gsap-trial type declarations and fix TS errors for Vercel build

---
+++ src/gsap-trial.d.ts
@@ -0,0 +1,11 @@
+declare module 'gsap-trial/SplitText' {
+  export const SplitText: any;
+}
+
+declare module 'gsap-trial/ScrollSmoother' {
+  export const ScrollSmoother: any;
+}
+
+declare module 'gsap-trial' {
+  export * from 'gsap';
+}
